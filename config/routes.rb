9e0b1b8676c3f9ff9747d88130e5017930d9b106
Reese Marshall <reese.amarshall@gmail.com>
2023-03-02 00:06:09 -0500

    commit tree acabe6b89739e380774c26ca6f35204581ab0138

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "SessionsController#create"
+  post "/logout", to: "SessionsController#destroy"
+
 end
